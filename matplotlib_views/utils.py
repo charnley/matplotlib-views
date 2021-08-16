7107835c26c07583a29f373a2b5d0ed0a084e278
Jimmy Charnley Kromann <jimmy@charnley.dk>
2021-08-16 20:07:29 +0200

    holiday update

--- matplotlib_views/utils.py
+++ matplotlib_views/utils.py
@@ -1,8 +1,9 @@
-import matplotlib
+from cycler import cycler
 from matplotlib import patheffects
+from matplotlib import pyplot as plt
+import matplotlib
 import numpy as np
 import pandas as pd
-from matplotlib import pyplot as plt
 
 
 def get_plot(n_ax=1, sharey=True, sharex=True):
@@ -30,6 +31,30 @@ def set_global_style(font_size=18):
     matplotlib.rcParams['ytick.major.width'] = 2
 
     return
+
+
+def set_colorblind_colors():
+
+    mpl.colors.ColorConverter.colors['r'] = hex2color('#e41a1c')
+    mpl.colors.ColorConverter.colors['b'] = hex2color('#377eb8')
+    mpl.colors.ColorConverter.colors['g'] = hex2color('#4daf4a')
+    mpl.colors.ColorConverter.colors['p'] = hex2color('#984ea3')
+    mpl.colors.ColorConverter.colors['y'] = hex2color('#ff7f00')
+
+
+def hex2color(s):
+    """
+    Function from MPL lib.
+
+    Take a hex string *s* and return the corresponding rgb 3-tuple
+    Example: #efefef -> (0.93725, 0.93725, 0.93725)
+    """
+    hexColorPattern = re.compile("\A#[a-fA-F0-9]{6}\Z")
+    # if not isinstance(s, basestring):
+    #     raise TypeError('hex2color requires a string argument')
+    if hexColorPattern.match(s) is None:
+        raise ValueError('invalid hex color string "%s"' % s)
+    return tuple([int(n, 16)/255.0 for n in (s[1:3], s[3:5], s[5:7])])
 
 
 def teach_style(font_size=18):
---
+++ matplotlib_views/vapnik.py
@@ -0,0 +1,83 @@
+
+from matplotlib import ticker
+from matplotlib_views import formats
+import numpy as np
+import re
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+from matplotlib import ticker
+
+
+def learning_curve_error(
+    ax,
+    xkeys,
+    ykeys,
+    x_range=None,
+    y_range=None,
+    border=[False, False, True, True],
+    loglog=True,
+    show_legend=True,
+):
+    """
+
+    """
+
+    if loglog:
+        ax.set_xscale('log')
+        ax.set_yscale('log')
+
+
+    ax.set_xticks(xkeys)
+    ax.set_yticks(ykeys)
+
+    if x_range is None:
+        ax.set_xlim((min(xkeys)*(1-0.1), max(xkeys)*(1+0.1)))
+    else:
+        ax.set_xlim(tuple(x_range))
+
+    if y_range is None:
+        ax.set_ylim((min(ykeys), max(ykeys)))
+    else:
+        ax.set_ylim(tuple(y_range))
+
+
+    ax.yaxis.set_major_formatter(ticker.FuncFormatter(formats.formatter_notrail))
+    ax.yaxis.set_minor_formatter(ticker.FuncFormatter(formats.formatter_off))
+
+    ax.xaxis.set_major_formatter(ticker.FuncFormatter(formats.formatter_int))
+    ax.xaxis.set_minor_formatter(ticker.FuncFormatter(formats.formatter_off))
+
+
+    # ax.xaxis.set_tick_params(width=1.2)
+    # ax.yaxis.set_tick_params(width=1.2)
+
+    if border:
+        # I like the css standard
+        spines = ax.spines.items()
+        for direction, spine in spines:
+
+            # spine.set_linewidth(1.2)
+
+            if direction == "top":
+                spine.set_visible(border[0])
+
+            if direction == "right":
+                spine.set_visible(border[1])
+
+            if direction == "bottom":
+                spine.set_visible(border[2])
+                spine.set_bounds(min(xkeys), max(xkeys))
+
+            if direction == "left":
+                spine.set_visible(border[3])
+                spine.set_bounds(min(ykeys), max(ykeys))
+
+
+    # Remove the small minor ticks in log-log plot
+    ax.minorticks_off()
+
+
+
+    return
